e7406d01b143ce182353700ee6492c1dc5aa1bd8
Rishi Ghan <hghan@apple.com>
2019-02-12 09:54:40 -0800

    Added loaders

--- webpack-config/webpack.dev.config.js
+++ webpack-config/webpack.dev.config.js
@@ -1,1 +1,15 @@
 const webpack = require("webpack");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
