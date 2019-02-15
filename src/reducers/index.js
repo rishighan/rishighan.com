77bf24ed75e239bf58882bd77aa9b96027cb16ed
Rishi Ghan <rishi@Rishis-iMac.local>
2019-02-15 09:42:41 -0800

    Refactored a reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
+import { LOAD_POSTS } from '../constants/action-types';
+
 const initialState = {
     posts: []
 };
 
 function rootReducer(state = initialState, action) {
+    if(action.type === LOAD_POSTS){ 
+        Object.assign(state, {
+            posts: state.posts.concat(action.payload)
+        })
+    }
     return state;
 }
 
