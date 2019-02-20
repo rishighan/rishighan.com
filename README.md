2cfe3ea4373c4a7343a5a930a5e64e391f8761bb
Rishi Ghan <hghan@apple.com>
2019-02-20 12:08:20 -0800

    Updated README

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This is a basic boilerplate built on:
 + Redux 6.0.0
 + Redux-thunk 2.3.0 
 + Webpack 4.29.3 configured with `"@babel/core": "^7.2.2"`
++ Support for `async/await`
+
+## Local development
+
+`nvm i 10.15.0 && nvm use 10.15.0`
+
+1. Spin up the local node server using `npm run dev` 
+2. Run `webpack` while watching for file changes using `npm run hot`
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import _ from "lodash";
 
-const ListItem = (props) => {
-    return( 
-        <ul>
-            {props.data.posts.map((post, idx) => 
-                <li key={ idx }>
-                    { post.title }
-                </li>
-            )} 
-        </ul>
-    )
-}
+const ListItem = (props) => (
+    <ul>
+        {props.data.posts.map((post, idx) => 
+            <li key={idx}>
+                {post.title}
+            </li>
+        )}
+    </ul>)
 
 export default ListItem;
