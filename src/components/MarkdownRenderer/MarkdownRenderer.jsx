19c332e14c0caed764185d65c11c63fe19062f3c
Rishi Ghan <hghan@apple.com>
2019-02-20 17:54:23 -0800

    Refactored markdown renderer

--- src/components/MarkdownRenderer/MarkdownRenderer.jsx
+++ src/components/MarkdownRenderer/MarkdownRenderer.jsx
@@ -2,19 +2,19 @@ import { PureComponent } from "react";
 import kramed from "kramed";
 import renderHTML from "react-render-html";
 import hljs from "highlight.js";
-import "highlight.js/styles/obsidian.css";
+import "highlight.js/styles/atom-one-dark.css";
 import _ from "lodash";
 
-kramed.setOptions({
-    highlight: (code, language) => language 
-        ? hljs.highlight(language, code).value
-        : hljs.highlightAuto(code).value,
-    gfm: true,
-    tables: true,
-    breaks: true,
-    smartLists: true,
-    smartypants: true,
-});
+let options = {
+    renderer: new kramed.Renderer({
+        highlight: code => hljs.highlightAuto(code).value,
+        gfm: true,
+        tables: true,
+        breaks: true,
+        smartLists: true,
+        smartypants: true,
+      })
+};
 
 class MarkdownRenderer extends PureComponent {
     static defaultProps = {
@@ -23,7 +23,7 @@ class MarkdownRenderer extends PureComponent {
 
     render() {
         const { text } = this.props;
-        return !_.isNull(text) ? renderHTML(kramed(text)) : null;
+        return !_.isNull(text) ? renderHTML(kramed(text, options)) : null;
     }
 }
 
