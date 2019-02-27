25a0ca4578db01013507a00e46dd46509e08ef79
Rishi Ghan <hghan@apple.com>
2019-02-27 14:31:36 -0800

    Added styles

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import MarkdownRenderer from '../MarkdownRenderer/MarkdownRenderer';
 
 const ListItem = props => (
-    <div className="column content is-two-thirds-desktop is-two-thirds-tablet is-mobile">
+    <div className="column content is-two-thirds-desktop is-two-thirds-tablet is-mobile is-full-mobile">
         {props.data.posts.map((post, idx) => <article key={ idx }>
             <header><h2>{ post.title }</h2></header>
             <section><MarkdownRenderer text={ post.content }/></section>
