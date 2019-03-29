95d531cd8e22c752ec15d3aeedfb47505379c1ac
Rishi Ghan <hghan@apple.com>
2019-03-29 14:40:57 -0700

    Building work page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { fetchPosts } from '../actions/index';
 
 class Work extends Component {
-    render() {
-        return (<div> Work </div>);
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (<div> Work </div>);
+  }
 }
+
+const mapDispatchToProps = dispatch => ({
+  fetchPosts() {
+    const options = {
+      callMethod: 'get',
+      callURIAction: 'findByTagName',
+      callParams: {
+        tagName: 'Projects',
+        pageOffset: 1,
+        pageLimit: 10,
+      },
+    };
+    dispatch(fetchPosts(options));
+  },
+});
+
 export default Work;
