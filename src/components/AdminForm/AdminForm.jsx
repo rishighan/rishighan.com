1fac48743327f6a17174b274b132e481fd316681
Rishi Ghan <rishi.ghan@gmail.com>
2019-04-11 23:31:33 -0700

    Added form styling, reducer and action type

--- src/components/AdminForm/AdminForm.jsx
+++ src/components/AdminForm/AdminForm.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form'
 
-const AdminForm = (props) => <React.Fragment>
+const AdminForm = (props) => <div className="column content is-two-thirds-tablet is-full-mobile">
     <Form
         // onSubmit={onSubmit}
         // validate={validate}
         render={({ handleSubmit, pristine, invalid }) => (
             <form>
                 <h2>Write a Post</h2>
-                <div className="field is-horizontal">
+                <div className="field">
                     <label className="field-label is-normal">Title</label>
                     <div className="control is-expanded">
                         <Field name="title" component="input" placeholder="Enter a Title" className="input" />
                     </div>
                 </div>
+
+                <div className="field">
+                    <div class="tags has-addons">
+                        <span class="tag is-info">Alex Smith</span>
+                        <a class="tag is-delete"></a>
+                    </div>
+                </div>
             </form>
         )} />
-</React.Fragment>
+</div>
 
 export default AdminForm;
