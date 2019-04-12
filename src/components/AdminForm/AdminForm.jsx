2173018f86761d51097cfc4ef63ed2eaeda1e45f
Rishi Ghan <hghan@apple.com>
2019-04-12 11:15:44 -0700

    Refactoring markup for tags

--- src/components/AdminForm/AdminForm.jsx
+++ src/components/AdminForm/AdminForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
 
-const AdminForm = (props) => <div className="column content is-two-thirds-tablet is-full-mobile">
+const AdminForm = props => <div className="column content is-two-thirds-tablet is-full-mobile">
     <Form
         // onSubmit={onSubmit}
         // validate={validate}
@@ -15,16 +15,22 @@ const AdminForm = (props) => <div className="column content is-two-thirds-tablet
                     </div>
                 </div>
 
-                <div className="field">
-                    <div class="tags has-addons">
-                        <span class="tag is-info">Highlight</span>
-                        <a class="tag is-delete"></a>
-                        <span class="tag is-info">General</span>
-                        <a class="tag is-delete"></a>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <div className="tags has-addons">
+                            <span className="tag is-info">Highlight</span>
+                            <a className="tag is-delete"></a>
+                        </div>
+                    </div>
+                    <div className="control">
+                        <div className="tags has-addons">
+                            <span className="tag is-info">General</span>
+                            <a className="tag is-delete"></a>
+                        </div>
                     </div>
                 </div>
             </form>
         )} />
-</div>
+</div>;
 
 export default AdminForm;
