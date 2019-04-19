7015e03e9d6a116968a8e716de189925c362968e
Rishi Ghan <rishi.ghan@gmail.com>
2019-04-19 01:10:42 -0700

    Added a hook to change tab content

--- src/components/AdminForm/AdminForm.jsx
+++ src/components/AdminForm/AdminForm.jsx
@@ -1,67 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Field } from 'react-final-form';
 import PropTypes from 'prop-types';
 
 const onSubmit = async (values) => {
-  console.log('submitted');
+    console.log('submitted');
 };
 
-const AdminForm = props => <div className="column content is-two-thirds-tablet is-full-mobile">
-    <Form
-        onSubmit={onSubmit}
-        initialValues={
-            {
-              ...props.formData,
-            }
-        }
-        render={({
-          handleSubmit, pristine, invalid,
-        }) => (
-                <form>
-                    <h3>Write a Post</h3>
-                    <div className="field">
-                        <label className="field-label is-normal">Title</label>
-                        <div className="control is-expanded">
-                            <Field name="title" component="input" placeholder="Title for this content" className="input" />
-                        </div>
-                    </div>
+const contentDirectory = {
+    content: <div>coa</div>,
+    preview: <div>preview</div>
+}
 
-                    {/* Edit mode only */}
-                    <div className="field">
-                        <label className="field-label is-normal">Slug</label>
-                        <div className="control is-expanded">
-                            <Field name="slug" component="input" className="input" />
-                        </div>
-                    </div>
-
-                    <div className="field is-grouped">
-                        {props.formData.tags.map((tag, idx) => <div className="control" key={idx}>
-                            <div className="tags has-addons">
-                                <span className="tag is-link">{tag.id}</span>
-                                <a className="tag is-delete"></a>
+function AdminForm(props) {
+    const [tabContent, changeTab] = useState(contentDirectory['content']);
+    return (
+        <div className="column content is-two-thirds-tablet is-full-mobile">
+            <Form
+                onSubmit={onSubmit}
+                initialValues={
+                    {
+                        ...props.formData,
+                    }
+                }
+                render={({
+                    handleSubmit, pristine, invalid,
+                }) => (
+                        <form>
+                            <h2>Write a Post</h2>
+                            <div className="field">
+                                <label className="field-label is-normal">Title</label>
+                                <div className="control is-expanded">
+                                    <Field name="title" component="input" placeholder="Title for this content" className="input" />
+                                </div>
                             </div>
-                        </div>)}
-                    </div>
 
-                    <div className="field">
-                        <label className="field-label is-normal">Content</label>
-                        <div className="control is-expanded">
-                            <Field name="content" component="textarea" placeholder="Write" className="textarea" rows="14" />
-                        </div>
-                    </div>
+                            {/* Edit mode only */}
+                            <div className="field">
+                                <label className="field-label is-normal">Slug</label>
+                                <div className="control is-expanded">
+                                    <Field name="slug" component="input" className="input is-disabled" />
+                                </div>
+                            </div>
 
-                    <div className="field">
-                        <label className="field-label is-normal">Excerpt</label>`
-                        <div className="control is-expanded">
-                            <Field name="title" component="textarea" placeholder="Write" className="textarea is-small" rows="3" />
-                        </div>
-                    </div>
-                </form>
-        )} />
-</div>;
+                            <div className="field is-horizontal">
+                                <label className="field-label is-normal">Tags</label>
+                                {props.formData.tags.map((tag, idx) => <div className="control" key={idx}>
+                                    <div className="tags has-addons">
+                                        <span className="tag is-link">{tag.id}</span>
+                                        <a className="tag is-delete"></a>
+                                    </div>
+                                </div>)}
+                            </div>
+
+                            <div className="tabs">
+                                <ul>
+                                    <li className="is-active" onClick={() => changeTab(contentDirectory['content'])}>
+                                        <a>Content</a>
+                                    </li>
+                                    <li onClick={() => changeTab(contentDirectory['preview'])}>
+                                        <a>Preview</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div id="tab-content">
+                                {tabContent}
+                            </div>
+
+                            <div className="field">
+                                <label className="field-label is-normal">Excerpt</label>
+                                <div className="control is-expanded">
+                                    <Field name="title" component="textarea" placeholder="Write" className="textarea is-small" rows="3" />
+                                </div>
+                            </div>
+                        </form>
+                    )} />
+        </div>);
+}
+
+
+
+
 
 AdminForm.propTypes = {
-  formData: PropTypes.object,
+    formData: PropTypes.object,
 };
 
 export default AdminForm;
