12e007b1a5a4cca6a92ff7217a2ff912eecebd05
Rishi Ghan <rishi.ghan@gmail.com>
2020-01-05 23:36:20 -0800

    Added autosave component

---
+++ src/components/Autosave/Autosave.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FormSpy } from 'react-final-form';
+import _ from 'lodash';
+import diff from 'object-diff';
+
+class Autosave extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { values: props.values, submitting: false };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+    this.timeout = setTimeout(this.save, this.props.debounce);
+  }
+
+  save = async () => {
+    if (this.promise) {
+      await this.promise;
+    }
+    const { values, save } = this.props;
+
+    // This diff step is totally optional
+    const difference = diff(this.state.values, values);
+    if (!_.isEmpty(Object.keys(difference))) {
+      // values have changed
+    console.log(difference)
+      this.setState({ submitting: true, values })
+      this.promise = save(difference)
+      await this.promise
+      delete this.promise
+      this.setState({ submitting: false });
+    }
+  }
+
+  render() {
+    // This component doesn't have to render anything, but it can render
+    // submitting state.
+    return (
+      this.state.submitting && <div className="submitting">Submitting...</div>
+    )
+  }
+}
+
+// Make a HOC
+// This is not the only way to accomplish auto-save, but it does let us:
+// - Use built-in React lifecycle methods to listen for changes
+// - Maintain state of when we are submitting
+// - Render a message when submitting
+// - Pass in debounce and save props nicely
+export default props => (
+  <FormSpy {...props} subscription={{ values: true }} component={Autosave} />
+)
