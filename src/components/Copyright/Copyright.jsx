d7c95a84a352ac45a02be68d6160ce9db26766f6
Rishi Ghan <rishi.ghan@gmail.com>
2020-06-03 14:00:59 -0700

    © Copyright component - Build
    1. Added icons
    2. Added license information

--- src/components/Copyright/Copyright.jsx
+++ src/components/Copyright/Copyright.jsx
@@ -7,18 +7,26 @@ const Copyright = (props) => {
       <small>
         &copy; Copyright {moment().year()}, {props.options.entity}
         <p>
-          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+          <a
+            rel="license"
+            href="http://creativecommons.org/licenses/by-sa/4.0/"
+          >
             <img
               alt="Creative Commons License"
               style={{ borderWidth: 0 }}
               src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png"
             />
           </a>
+
           <br />
-  {props.options.articleToUse} {props.options.copyrightObject} is licensed under a{" "}
-          <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+          {props.options.articleToUse} {props.options.copyrightObject} is
+          licensed under a{" "}
+          <a
+            rel="license"
+            href="http://creativecommons.org/licenses/by-sa/4.0/"
+          >
             Creative Commons Attribution-ShareAlike 4.0 International License
-        </a>
+          </a>
         </p>
       </small>
     </div>
