e3ead6c6be419f6466bdb7369e57cf317a4c8e4f
Rishi Ghan <rishi.ghan@gmail.com>
2020-09-12 01:39:40 -0700

    🔐 Switched posts-api to HTTPS

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,4 +1,4 @@
 export const SERVICE_BASE_URI = "http://services.rishighan.com/";
-export const POSTS_SERVICE_URI = 'http://localhost/api/v1/posts/';
+export const POSTS_SERVICE_URI = 'https://posts.services.rishighan.com/api/v1/posts/';
 export const ASSETS_SERVICE_URI = 'http://localhost/assets/api/';
 export const USER_SERVICE_URI = 'http://localhost/api/user';
